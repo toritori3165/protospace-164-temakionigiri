16dbf4698bbb8e723aa657d53dc25699f78bb627
Ebisawa1016 <ebisawa.yusuke1016@gmail.com>
2024-01-22 12:35:45 +0900

    コメント投稿機能と詳細確認

--- app/controllers/coments_controller.rb
+++ app/controllers/coments_controller.rb
@@ -1,2 +1,11 @@
 class ComentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/prototypes/#{comment.prototyps.id}"  # コメントと結びつくツイートの詳細画面に遷移する
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:rototype_id])
+  end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,10 @@
 class PrototypesController < ApplicationController
   def index
-  end  
+  end
+  
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
 end
