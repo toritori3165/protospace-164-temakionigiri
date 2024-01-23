82cbf58dd409685809c15953c83f91f528fef646
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-01-23 16:59:30 +0900

    プロトタイプ削除機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,16 +1,15 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:edit, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
   end
-  
+
   def show
     @comment = Comment.new
-    @prototype = Prototype.find(params[:id])
     @comments = @prototype.comments.includes(:user)
-    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -26,10 +25,32 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    redirect_to prototype_path(prototype.id)
+  end
+
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def move_to_index
