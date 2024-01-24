8426efcc2a26aec3803f59a8370e6fd643cac5b4
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-01-24 16:00:44 +0900

    最終修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -38,9 +38,12 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    redirect_to prototype_path(prototype.id)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
